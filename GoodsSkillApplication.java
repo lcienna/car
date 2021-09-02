48732f65f0c48b2d4e3e54cc72b3b7d34e7daf49
lcienna <34996768+lcienna@users.noreply.github.com>
2021-09-02 11:13:06 +0800

    Add files via upload

---
+++ GoodsSkillApplication.java
@@ -0,0 +1,13 @@
+package com.example.goodsskill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsSkillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodsSkillApplication.class, args);
+    }
+
+}
